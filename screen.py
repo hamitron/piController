8c25441b4ad51ce87d7e88c90bdadfcf07122256
Hamilton Q. Karl <hqkarl@gmail.com>
2019-04-13 21:45:32 -0700

    renaming. screen py will probs be deleted soon

--- screen.py
+++ screen.py
@@ -9,15 +9,15 @@ from PIL import ImageFont
 
 pin=40
 resetPin=38
-boardAddress=0x3d
-oledAddress=0x3c
+boardAddress=0x3D
+oledAddress=0x3C
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(resetPin, GPIO.OUT)
 
-display = Adafruit_SSD1306.SSD1306_128_64(rst=None, gpio=GPIO, i2c_address=oledAddress)
-font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf', 18)
+display = Adafruit_SSD1306.SSD1306_128_64(rst=None, gpio=GPIO, i2c_bus=1, i2c_address=oledAddress)
+font = ImageFont.truetype('/usr/share/fonts/truetype/droid/DroidSansFallbackFull.ttf', 8)
 image = Image.new('1', (128,64))
 draw = ImageDraw.Draw(image)
 
@@ -34,13 +34,16 @@ def revert():
 
 def read(address):
     with SMBusWrapper(1) as bus:
-        b = bus.read_byte_data(boardAddress, address)
+        b = bus.read_i2c_block_data(boardAddress, address, 1)
         return b
 
 def displayText(txt):
+    print txt
+
+def oledDisplay():
     display.clear()
     draw.rectangle((0,0,128,64), outline=1, fill=0)
-    draw.text((30,0), txt, font=font, fill=255)
+    draw.text((30,0), "text", font=font, fill=255)
     display.image(image)
     display.display()
 
@@ -64,4 +67,5 @@ display.display()
 GPIO.add_event_detect(pin, GPIO.BOTH, callback=pinCallback)
 while True:
     print "heartbeat"
-    time.sleep(10)
+    time.sleep(3)
+    oledDisplay()
